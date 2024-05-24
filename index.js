8ddd24713891a08eb62e1b39065d3d82caf47d09
musfiqrabby <musfiqrabby69@gmail.com>
2024-05-24 19:17:52 +0600

    update deleted api calls

--- index.js
+++ index.js
@@ -131,6 +131,15 @@ async function run() {
       res.send(result);
     });
 
+
+    app.get('/menu/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await menuCollection.findOne(query);
+      res.send(result);
+    })
+
+
     app.post('/menu', verifyToken, verifyadmin, async(req, res) =>{
       const item = req.body;
       const result = await menuCollection.insertOne(item);
@@ -141,6 +150,32 @@ async function run() {
       const result = await reviewsCollection.find().toArray();
       res.send(result);
     })
+
+    app.patch('/menu/:id', async(req, res) => {
+      const item = req.body;
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updatedDoc = {
+        $set: {
+          name: item.name,
+          category: item.category,
+          price: item.price,
+          recipe: item.recipe,
+          Image: item.image,
+        }
+      }
+      const result = await menuCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
+
+   app.delete('/menu/:id', verifyToken, verifyadmin, async (req, res) => {
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await menuCollection.deleteOne(query);
+    res.send(result)
+   })
+
 
     //carts collections
     app.get('/carts', async(req, res) => {
