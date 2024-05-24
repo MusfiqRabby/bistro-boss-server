13c90ceb02dc1762411e39f6a77f834e0662436d
musfiqrabby <musfiqrabby69@gmail.com>
2024-05-24 12:04:54 +0600

    five part start

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
@@ -26,12 +27,113 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
+
+    const userCollection = client.db("bistroDb").collection('users');
     const menuCollection = client.db("bistroDb").collection('menu');
     const reviewsCollection = client.db("bistroDb").collection('reviews');
     const cartCollection = client.db("bistroDb").collection('cart');
 
+
+    // jwt related api
+    app.post('/jwt', async(req, res) =>{
+      const user =req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+      res.send({token})
+    })
+
+
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      // console.log('inside verify token', req.headers.authorization);
+      if(!req.headers.authorization){
+        return res.status(401).send({message: 'unauthorized access'})
+      }
+      const token = req.headers.authorization.split(' ')[1]
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err){
+          return res.status(401).send({message: 'unauthorized access'})
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
+    // use verify admin after verifyToken
+     const verifyadmin = async(req, res, next) =>{
+      const email = req.decoded.email;
+      const query = {email: email};
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if(!isAdmin){
+        return res.status(403).send({message: 'forbidden access'});
+      }
+      next();
+     }
+
+
+    // users related api 
+    app.get('/users', verifyToken, verifyadmin, async(req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.get('/users/admin/:email', verifyToken, async(req, res) => {
+      const email = req.params.email;
+      if(email !== req.decoded.email){
+        return res.status(403).send({message: 'forbidden access'})
+      }
+      const query = {email: email};
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if(user){
+        admin = user?.role == 'admin';
+      }
+      res.send({admin});
+    })
+
+
+    app.post('/users', async(req, res) => {
+      const user = req.body;
+      // insert email if user dosent exists:
+      const query = {email: user.email}
+      const existingUser = await userCollection.findOne(query)
+      if(existingUser){
+        return res.send({message: 'user Already exists', insertedId: null})
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+
+    app.patch('/users/admin/:id', verifyToken, verifyadmin, async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc)
+      res.send(result);
+    })
+
+
+    app.delete('/users/:id', verifyToken, verifyadmin, async(req, res) => {
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query);
+      res.send(result)
+    })
+
+    // menu relatade apis
     app.get('/menu', async(req, res) => {
       const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post('/menu', verifyToken, verifyadmin, async(req, res) =>{
+      const item = req.body;
+      const result = await menuCollection.insertOne(item);
       res.send(result);
     })
 
@@ -76,7 +178,7 @@ run().catch(console.log);
 
 
 app.get('/', (req, res) => {
-    res.send('boss is sitting');
+    res.send('boss is setting...');
 })
 
 app.listen(port, () => {
